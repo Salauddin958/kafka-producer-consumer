d2215104fb4532d7e7eecf228d016eb0c88cca05
Mohammed Salauddin <Salauddinmd1312@gmail.com>
2021-08-04 16:27:54 +0530

    changes in kafka controller and servive

--- src/main/java/com/example/kafka/controller/KafkaController.java
+++ src/main/java/com/example/kafka/controller/KafkaController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.example.kafka.service.ProducerService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/weather")
 public final class KafkaController {
@@ -30,8 +33,9 @@ public final class KafkaController {
         this.producerService = producerService;
     }
 
-    @PostMapping(value = "/daily/{location}")
+    @GetMapping(value = "/daily/{location}")
     public ResponseEntity<String> sendDailyWeatherDetails(@PathVariable("location") String location) {
+    	System.out.println("inside daily kafka producer");
     	Map<String, String> map = new HashMap();
     	map.put("location", location);
     	String response = restTemplate.getForObject(BASE_URL+"daily/{location}", String.class, map);
@@ -39,8 +43,9 @@ public final class KafkaController {
         return new ResponseEntity<String>(response, HttpStatus.OK);
     }
     
-    @PostMapping(value = "/historic/{location}")
+    @GetMapping(value = "/historic/{location}")
     public ResponseEntity<String> sendHistoricWeatherDetails(@PathVariable("location") String location) {
+    	System.out.println("inside historic kafka producer");
     	Map<String, String> map = new HashMap();
     	map.put("location", location);
     	String response = restTemplate.getForObject(BASE_URL+"historic/{location}", String.class, map);
@@ -48,8 +53,9 @@ public final class KafkaController {
         return new ResponseEntity<String>(response, HttpStatus.OK);
     }
     
-    @PostMapping(value = "/future/{location}")
+    @GetMapping(value = "/future/{location}")
     public ResponseEntity<String> sendFutureWeatherDetails(@PathVariable("location") String location) {
+    	System.out.println("inside future kafka producer");
     	Map<String, String> map = new HashMap();
     	map.put("location", location);
     	String response = restTemplate.getForObject(BASE_URL+"future/{location}", String.class, map);
--- src/main/java/com/example/kafka/service/ConsumerService.java
+++ src/main/java/com/example/kafka/service/ConsumerService.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public final class ConsumerService {
-    private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
-
-    @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.consumerGroupId}")
-    public void consume(String message) {
-        logger.info(String.format("$$$$ => Consumed message: %s", message));
-    }
+//    private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
+//
+//    @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.consumerGroupId}")
+//    public void consume(String message) {
+//        logger.info(String.format("$$$$ => Consumed message: %s", message));
+//    }
 }
